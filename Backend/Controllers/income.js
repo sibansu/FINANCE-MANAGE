c8807bd5749a2e4145357e4c2d7695f14b9c7ad4
Sibansu <mishrasibansu16@gmail.com>
2023-09-18 01:16:52 +0530

    comments added

--- Backend/Controllers/income.js
+++ Backend/Controllers/income.js
@@ -11,8 +11,8 @@ const addIncome = async(req, res)=>{
 
     console.log(income)
     try {
-        //added
         //addIncome checks
+        //comments added
         if(!title || !amount || !date || !category || !description){
             return res.status(400).json({message: "All fields are mandatory"})
         }
