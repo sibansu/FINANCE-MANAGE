2eb16f9ce0d95b7fda32b7f47cb21ae61105add3
Sibansu <mishrasibansu16@gmail.com>
2023-09-20 00:15:56 +0530

    Added styled components

---
+++ frontend/src/Styles/GlobalStyles.js
@@ -0,0 +1,29 @@
+import {createGlobalStyle} from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+    }
+
+    :root{
+        --primary-color: #222260;
+        --primary-color2: 'color: rgba(34, 34, 96, .6)';
+        --primary-color3: 'color: rgba(34, 34, 96, .4)';
+        --color-green: #42ad00;
+        --color-grey: #aaa;
+        --color-accent: #F56692;
+        --color-delete: #FF0000 
+    }
+
+    body{
+        font-family: 'Roboto', sans-serif;
+        font-family: 'Roboto Condensed', sans-serif;
+        /* background-color: red; */
+        font-size: clamp(1rem, 1.5vw, 1.2rem);
+        overflow: hidden;
+        color: rgba(34,34,96,.6);
+    }
+`;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import { GlobalStyles } from './Styles/GlobalStyles';
+// import { globalState } from './Styles/GlobalStyles';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyles></GlobalStyles>
     <App />
   </React.StrictMode>
 );
